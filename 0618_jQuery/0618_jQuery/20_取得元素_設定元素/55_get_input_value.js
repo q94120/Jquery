c6816692a5d591e612c3330c30331de4375f448a
Leo <q94120@gmail.com>
2024-06-19 16:27:26 +0800

    0619 16:30

--- 0618_jQuery/0618_jQuery/20_取得元素_設定元素/55_get_input_value.js
+++ 0618_jQuery/0618_jQuery/20_取得元素_設定元素/55_get_input_value.js
@@ -1,77 +1,7 @@
-// 10. 如果是這樣寫的話，有錯誤訊息，為什麼？
-btnGet.onclick = function () {
-    console.log("OK");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // 10. 如果是這樣寫的話，有錯誤訊息，為什麼？
+// btnGet.onclick = function () {
+//     console.log("OK");
+// }
 
 
 
@@ -80,18 +10,28 @@ btnGet.onclick = function () {
 // 11. 畫面準備好以後才開始執行
 $(document).ready(function () {
     btnGet.onclick = function () {
-
         // 20. 取得 姓名欄位 (使用 JavaScript 的方式)
         // 21. 取得 姓名欄位 (使用 jQuery 的方式)
-
+        let x = document.getElementById('userName').value ; // 對於input來說，他沒有innerTEXT的概念，因為他沒有結束標籤，所以他得要用value
+        let y = $('#userName').prop('value');
+        let z = $('#userName').val();
+        // 如果要抓取的元素是有value屬性的，那就可以使用val
+        console.log(x);
+        console.log(y);
+        console.log(z);
 
         // 30. 取得 地址欄位 (使用 JavaScript 的方式)
         // 31. 取得 地址欄位 (使用 jQuery 的方式)
-
+        let q = document.getElementById('address').value;
+        let w = $('#address').val();
+        console.log(q);
+        console.log(w);
 
         // 40. 取得 年紀欄位 (使用 JavaScript 的方式)
         // 41. 取得 年紀欄位 (使用 jQuery 的方式)
+        let r = document.getElementById('age').value;
 
+        console.log(r);
     }
 });
 
